f266dc42393285563d4ca4172e4af3aa207d57e7
Aleen Alhussain <169069633+AleenAlhussain@users.noreply.github.com>
2026-04-27 19:08:18 +0300

    edit files

--- public/js/app.js
+++ public/js/app.js
@@ -37292,8 +37292,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\xampp\htdocs\laravel-7-multi-auth\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\xampp\htdocs\laravel-7-multi-auth\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/hadiabouammar/Documents/GitHub/store/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/hadiabouammar/Documents/GitHub/store/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
